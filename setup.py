d76ea4a8b2837bdc3b9a83bcf0b0cb3131e55c13
shubhamgapat19 <gapatshubham19@gmail.com>
2026-04-03 16:20:16 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="shubhamgapat19",
+    author_email="gapatshubham19@gmail.com",
+    packages=find_packages()
+)
